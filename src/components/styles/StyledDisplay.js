71ec3e729e3181ecb98ddecde1e3bdbd93d8cdc0
Julia Moses <jslmoses@gmail.com>
2019-10-21 23:20:17 -0400

    start button added

--- src/components/styles/StyledDisplay.js
+++ src/components/styles/StyledDisplay.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledDisplay = styled.div`
+export const StyledDisplay = styled.div `
 	box-sizing: border-box;
 	display: flex;
 	align-items: center;
